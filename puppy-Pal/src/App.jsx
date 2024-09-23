4f03eccfb29f9a174e52b55ce158cb06c7dd3fb6
Alexis Sushko <alexis.sushko@gmail.com>
2024-09-23 18:47:52 -0400

    Imported use State from react. Added a useState with the value puppies and the function setPuppies. I console logged the puppies array. I mapped over each puppy by getting their id and returning theie name

--- puppy-Pal/src/App.jsx
+++ puppy-Pal/src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-
+const [puppies, setPuppies] = useState(puppyList);
+console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+    <div className = "app">
+ {
+  puppies.map((puppy) => {
+    return <p key={puppy.id}>{puppy.name}</p>;
+ })
+}
+</div>
+  );
 }
 
+export default App
